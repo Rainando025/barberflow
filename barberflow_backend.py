b4144e0b0bfb29cc58cf2e60008f12f83337f8c0
Rainando025 <rainando123@gmail.com>
2025-10-31 12:06:08 -0300

    Update barberflow_backend.py

--- barberflow_backend.py
+++ barberflow_backend.py
@@ -522,6 +522,7 @@ HTML_TEMPLATE = f"""
         body {{
             font-family: 'Inter', sans-serif;
             background-color: #f3f4f6;
+            background: url('{{{{ url_for('https://raw.githubusercontent.com/Rainando025/barberflow/refs/heads/main/barbearia5.jpg') }}}}') no-repeat center center / cover;
         }}
         #loading-overlay {{
             z-index: 50;
@@ -1682,6 +1683,7 @@ HTML_TEMPLATE = f"""
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
